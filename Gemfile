8bcd2b3cd3c14f62f21de70f443755cf14ff5933
Gabrijela Gavric and Qudsia Ahmad <gabrijela+qudsia@>
2017-12-13 13:52:59 -0800

    unsucessful authentication devise gem

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem 'rails', '~> 5.1.4'
 gem 'pg', '~> 0.18'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
+gem 'rack-cors'
+gem 'devise_token_auth'
 
 group :development, :test do
   gem 'rspec-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root :to => 'desinations#index'
+  mount_devise_token_auth_for 'User', at: 'auth'
+  root :to => 'destinations#index'
   resources :destinations do
     resources :reviews, :except => [:show, :index]
   end
